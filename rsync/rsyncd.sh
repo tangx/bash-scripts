1cc13e2b70a01d0cbf3ccfca2d59e2c79e64471a
uyinn <uyinn@live.com>
2015-08-18 18:02:06 +0800

    添加rsync配置文件及启动脚本
    添加rsync配置文件及启动脚本

---
+++ rsync/rsyncd.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+# rsync boot file
+
+[ -f /etc/init.d/functions ] && . /etc/init.d/functions
+
+# BASEDIR=$(dirname $(which rsync))
+BASEDIR=/usr/bin
+PIDFILE=/var/run/rsyncd.pid
+CONF_FILE=/etc/rsyncd/rsyncd.conf
+
+
+function DoAction()
+{
+  [ $? -eq 0 ] && \
+  action "$doAct" /bin/true || \
+  action "$doAct" /bin/false
+}
+
+function start()
+{
+  doAct="Starting Rsync Daemon  "
+  [ -z "$CONF_FILE" ] && CONF_FILE=/etc/rsyncd.conf
+  [ -e $PIDFILE ]  || $BASEDIR/rsync --config=$CONF_FILE --daemon  
+  DoAction 
+  
+}
+
+function stop()
+{
+  doAct="Stoping Rsync Daemon"
+  [ -e $PIDFILE ] && kill -15 $(cat $PIDFILE) && rm -f $PIDFILE
+  DoAction
+}
+
+function status()
+{
+  [ -e $PIDFILE ] && \
+  echo "Rsync is Running [ $(cat $PIDFILE) ]" || \
+  echo "Rsync is Stopped"
+}
+
+case $1 in 
+start)  start ;;
+stop)  stop ;;
+restart) stop && sleep 2 ;start ;;
+status)  status ;;
+*)    echo "Usage: $0 [start|stop|restart|status]" ;;
+esac
+  
