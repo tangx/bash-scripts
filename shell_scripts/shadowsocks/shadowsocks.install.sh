13c30275baafa7a44fa70e0e8173525d983a08f3
HsinTang <uyinn@live.com>
2016-07-29 10:49:21 +0800

    update
    use ec2 as first argument to install shadows on aws

--- shell_scripts/shadowsocks/shadowsocks.install.sh
+++ shell_scripts/shadowsocks/shadowsocks.install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #
+
+
+timestamp=$(date+ %s)
+
 # wget https://raw.githubusercontent.com/uyinn/bash-scripts/master/shell_scripts/shadowsocks/shadowsocks.install.sh && sh shadowsocks.install.sh
   which pip > /dev/null 2>&1 || {
     wget -c https://bootstrap.pypa.io/get-pip.py
@@ -14,16 +18,24 @@
   SHADOW_DIR=/usr/local/shadowscoks/
   mkdir -p $SHADOW_DIR
   cd $_
+  
+  [ -f ss.json.conf ] && cp -a ss.json.conf{,.ori_$timestamp}
   wget https://raw.githubusercontent.com/uyinn/bash-scripts/master/shell_scripts/shadowsocks/ss.json.conf
 
-  PUB_IP=$(curl -s ip.cn | grep -oP '(\d+\.)+\d+')
-  sed -i "s/PUBLIC_IP/$PUB_IP/" ss.json.conf 
 
   # mv ss.json.conf /etc/shadowsocks/
 
-  
+  case $1 in 
+  ec2)
+    PUB_IP=$(ifconfig eth0 |grep -oP '(\d+\.)+\d+' | head -n 1)
+    ;;
+  *)
+    PUB_IP=$(curl -s ip.cn | grep -oP '(\d+\.)+\d+')
+    ;;
+  sed -i "s/PUBLIC_IP/$PUB_IP/" ss.json.conf 
   
 # 获取启动文件
+  [ -f shadowsocksd.sh ] && cp -a shadowsocksd.sh{,.ori_$timestamp}
   wget https://raw.githubusercontent.com/uyinn/bash-scripts/master/shell_scripts/shadowsocks/shadowsocksd.sh
   chmod +x shadowsocksd.sh
   sed -i "s@SSSERVER_BIN@$(which ssserver)@" shadowsocksd.sh 
