2addd389f49fadf6288260bf079173459e140704
HsinTang <uyinn@live.com>
2017-01-20 10:54:59 +0800

    upgrade pip install

--- shell_scripts/pip_install.sh
+++ shell_scripts/pip_install.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 #
 # install pip
+# test on centos6 and centos 7
 #
 
-yum -y install python
-# wget -c https://raw.github.com/pypa/pip/master/contrib/get-pip.py
-wget -c https://bootstrap.pypa.io/get-pip.py
-python get-pip.py 
+country=$1
 
-mkdir -p .pip/
+function __install_pip()
+{
+    yum -y install python
+    # wget -c https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+    wget -c https://bootstrap.pypa.io/get-pip.py
+    python get-pip.py 
 
-cat > .pip/pip.conf <<EOF
+}
+
+function __ustc_mirrors()
+{
+mkdir -p ~/.pip/
+cat > ~/.pip/pip.conf <<EOF
 [global]
 index-url = https://mirrors.ustc.edu.cn/pypi/web/simple
 format = columns
 EOF
+}
+
+
+# if pip is not exist ,then install it
+which pip || __install_pip
+
+
+# set the default source mirror 
+case $country in 
+[cC][nN]) __ustc_mirrors ;;
+# [cC][nN]) __ustc_mirrors ;;
+esac
 
